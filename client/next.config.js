f6d63be13704eeb487a109e9b2131a063b40600d
atkinsonholly <atkinsonholly@gmail.com>
2023-01-08 22:06:15 +0000

    try: cors fix

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,8 @@ module.exports = {
     async headers() {
       return [
         {
-          source: "*",
+          // matching all API routes
+          source: "/api/asset/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "https://funny-cascaron-71bcc0.netlify.app/" },
